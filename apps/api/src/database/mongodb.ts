e89f0f9faa8a8358def3726e066e58a4039b44bf
SANTHOSH N <santhoshdeveloper.mail@gmail.com>
2026-06-28 22:26:31 +0530

    Product change in file to ProductForm file.

--- apps/api/src/database/mongodb.ts
+++ apps/api/src/database/mongodb.ts
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 import { env } from "node:process";
 
 const connectDB = async () => {
+  
   try {
     await mongoose.connect(env.MONGO_URI as string);
-
     console.log("MongoDB Connected");
+
   } catch (error) {
     console.log(error);
     process.exit(1);
